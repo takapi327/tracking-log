431ae01f12470ae71ab16d6e2abb161fd44b9c65
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-09-27 19:49:45 +0900

    Create LogbackJsonEncoder

---
+++ src/main/scala/trackingLog/mvc/LogbackJsonEncoder.scala
@@ -0,0 +1,29 @@
+package trackingLog.mvc
+
+import play.api.libs.json.Json
+
+import ch.qos.logback.core.LayoutBase
+import ch.qos.logback.core.CoreConstants
+import ch.qos.logback.classic.spi.ILoggingEvent
+
+class LogbackJsonEncoder extends LayoutBase[ILoggingEvent] {
+
+  def doLayout(event: ILoggingEvent): String = {
+    Json.obj(
+      "timeStamp" -> event.getTimeStamp,
+      "level"     -> event.getLevel.toString,
+      "maker"     -> event.getMarker.toString,
+      "thread"    -> event.getThreadName,
+      "message"   -> event.getMessage,
+      "throwable" -> event.getThrowableProxy.toString,
+      "class"     -> event.getClass.toString,
+      "argument"  -> event.getArgumentArray.toString,
+      "callerData" -> event.getCallerData.toString,
+      "loggerContextVO" -> event.getLoggerContextVO.toString,
+      "formattedMessage" -> event.getFormattedMessage,
+      "mdcPropertyMap" -> event.getMDCPropertyMap.toString,
+      "loggerName" -> event.getLoggerName,
+      "coreConstants" -> CoreConstants.LINE_SEPARATOR
+    ).toString()
+  }
+}
